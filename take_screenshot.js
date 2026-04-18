416431ab19ced1a2eca33906082d9481352af65e
live4golf <mark@markhelsby.com>
2026-04-18 08:05:14 +0700

    Initial commit: IA Confluence Strategy v1

---
+++ take_screenshot.js
@@ -0,0 +1,11 @@
+﻿const CDP = require('chrome-remote-interface');
+async function run() {
+  const client = await CDP({ port: 9222, target: '7CC57DE01F78319EEF6502536AA0058B' });
+  const { Page } = client;
+  await Page.enable();
+  const { data } = await Page.captureScreenshot({ format: 'png', quality: 100 });
+  require('fs').writeFileSync('C:\\\\Users\\\\mark\\\\Documents\\\\AnitGravity\\\\Trading View\\\\chart_screenshot.png', Buffer.from(data, 'base64'));
+  console.log('Screenshot saved');
+  await client.close();
+}
+run().catch(console.error);
